f9ca66126cb91eaaaffda64ea913b1ad1b8f9fab
miryam-git-lang <miryamea@code.edu.az>
2026-02-01 19:44:48 +0400

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+int numb = 25;
+int i = 1;
+bool check = false;
+
+while (i < numb)
+{
+	i++;
+	if (numb % i == 0)
+	{
+		check = true;
+		break;
+	}
+}	
+
+if (check == true)
+{
+	Console.WriteLine("reqem murekkebdir");
+}
+else
+{	
+	Console.WriteLine("reqem sadedir");
+}
+
+Console.ReadKey();
+
+
+
+
